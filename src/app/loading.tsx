d1997fb2ac558f42b4ccb18cf0f489010564c2f6
alves-leandro <l.alves85@live.com>
2024-03-03 23:55:44 -0300

    feat: loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,15 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <main className="body min-h-screen p-4 flex justify-center items-center">
+      <Box>
+        <Flex direction="column" alignItems="center">
+          <Box textAlign="center">
+            <Text>Loading</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </main>
+  );
 }
