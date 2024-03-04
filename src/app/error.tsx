f567f36b2f3232c3499a919e18a2d42e50a169b7
alves-leandro <l.alves85@live.com>
2024-03-03 23:39:28 -0300

    fix: error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,23 +1,30 @@
-'use client'
+"use client";
 
-import { Button } from "@chakra-ui/react";
-import { useEffect } from "react"
+import { Box, Button, Flex } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 interface ErrorProps {
-    error: Error,
-    reset: () => void
+  error: Error;
+  reset: () => void;
 }
 
-export default function Error({error, reset}:ErrorProps) {
-    useEffect(() => {
-        console.log(error);
-        
-    }, [error])
+export default function Error({ error, reset }: ErrorProps) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
 
-    return (
-        <>
+  return (
+    <main className="body min-h-screen p-4 flex justify-center items-center">
+      <Box>
+        <Flex direction="column" alignItems="center">
+          <Box textAlign="center">
             <h1>Sua página deu erro</h1>
-            <Button type="button" onClick={reset}>tentar novamente</Button>
-        </>
-    )
+            <Button mt={12} type="button" onClick={reset}>
+              Tentar novamente
+            </Button>
+          </Box>
+        </Flex>
+      </Box>
+    </main>
+  );
 }
